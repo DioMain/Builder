d71c43ffe2f55003e609f6ab4227bf592a823f2d
esse <redstrixw@gmail.com>
2023-03-10 23:31:38 +0300

    add yearBlock

--- builder/src/App.vue
+++ builder/src/App.vue
@@ -4,19 +4,26 @@
   p="стадионы, газопроводы, мосты, дамбы"></TheHead>
   <RequestLine h="САМЫЕ УМНЫЕ ПРОЕКТЫ" p="РЕАЛИЗУЕМ САМЫЕ СМЕЛЫЕ РЕШЕНИЯ В РОССИИ!" btn-text="ВАШ ЗАПРОС"></RequestLine>
   <WorkLabel imageUrl="Data/Icons/building.svg" Content="Some text"/>
+  <YearBlock image-url="Data/Images/arena.png" name="Газпром Арена" description="Мы сделали самую красивую арену в Европе.
+                                   Это открытие стало для нас прорывной точкой для разивтия на следующие десятилетия. 
+                                   Мы очень рады данному еву.">
+  </YearBlock>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import TheHead from './components/TheHead.vue';
 import RequestLine from './components/Request.vue';
-import WorkLabel from './components/WorkLabel.vue'
+import WorkLabel from './components/WorkLabel.vue';
+import YearBlock from './components/YearBlock.vue';
 
 export default defineComponent({
   name: 'App',
   components: {
     TheHead,
     WorkLabel,
+    RequestLine,
+    YearBlock
   }
 });
 </script>
@@ -32,6 +39,13 @@ export default defineComponent({
     width: 60%;
     height: 100%;
     margin: 0 auto 0 auto;
+}
+
+@font-face {
+  font-family: "OpenSansRegular";
+  src:url(../src/assets/fonts/OpenSans-Regular.ttf);
+  font-style: normal;
+  font-weight: normal;
 }
 
 @font-face {
---
+++ builder/src/components/YearBlock.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="project_card">
+        <img class="card_image" :src="imageUrl" alt="card image">
+        <div class="card_line"></div>
+        <p class="card_name">{{ name }}</p>
+        <p class="card_description">{{ description }}</p>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'YearBlock',
+  props: {
+    imageUrl: String,
+    name: String,
+    description: String
+  }
+});
+</script>
+
+<style scoped>
+    .project_card{
+        width: 350px;
+    }
+    .card_line{
+        width: 60px;
+        height: 6px;
+        background-color: #F7654A;
+
+        margin-top: 30px;
+        margin-bottom: 15px;
+    }
+    .card_name{
+        font-family: OswaldMedium;
+        font-size: 24px;
+        color: #404149;
+    }
+
+    .card_description{
+        font-family: OpenSansRegular;
+        font-size: 16px;
+        color: #999999;
+    }
+</style>
